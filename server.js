3fc2a2d7043ea641490f589a474502f4bae88a40
HarleyKwyn <kwyn.meagher@gmail.com>
2014-08-11 11:06:35 -0700

    Fixes pathing problem with server variable

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
+var publicDir = '/public' //for development or 'dist' for production
 
-app.use('/public', express.static(__dirname + '/public'));
+app.use(publicDir, express.static(__dirname + publicDir));
 
 app.all('/*', function(req, res, next) {
     // Just send the index.html for other files to support HTML5Mode
-    res.sendfile('public/index.html', { root: __dirname });
+    res.sendfile( publicDir+'/index.html', { root: __dirname });
 });
+
 app.listen(3000);
 console.log("Server listening on port 3000");
