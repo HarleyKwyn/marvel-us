29b878855860e0a3ed644792c3d94a60c71d419c
HarleyKwyn <kwyn.meagher@gmail.com>
2014-08-11 11:05:24 -0700

    Adds linting step in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
+    lint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
     livereload = require('gulp-livereload'),
     watch = require('gulp-watch'),
     nodemon = require('gulp-nodemon');
@@ -20,4 +22,27 @@ gulp.task('develop', function () {
     })
 });
 
-gulp.task('default', ['less']);
+gulp.task('lint', function(){
+  gulp.src([
+    './public/common/*.js',
+    './public/app/comics/*.js',
+    './public/app/creators/*.js',
+    './public/app/app.js'
+  ])
+  .pipe(lint())
+  .pipe(lint.reporter('default'));
+});
+
+gulp.task('bundle', function(){
+  gulp.src([
+    './public/common/*.js',
+    './public/app/comics/*.js',
+    './public/app/creators/*.js',
+    './public/app/app.js' 
+    ])
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('deploy', ['less', 'lint', 'bundle'])
+gulp.task('default', ['less', 'lint']);
